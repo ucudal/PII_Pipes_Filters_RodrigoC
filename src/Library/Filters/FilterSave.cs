d26e90f1e33d732bafd45b63b8df1455dd74283c
RodrigoCD16 <53954034+RodrigoCD16@users.noreply.github.com>
2022-06-05 21:07:42 -0300

    Ejercicio2

---
+++ src/Library/Filters/FilterSave.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+
+namespace CompAndDel.Filters
+{
+    /// <summary>
+    /// Un filtro que recibe una imagen y retorna una copia de la misma en un directorio.
+    /// </remarks>
+    public class FilterSave : IFilter
+    {   
+        
+        public string Path {get; set;}
+        /// <summary>
+        /// Método añadido para poder cambiar el directorio en el que se 
+        /// guarda la copia de la imagen.
+        /// </summary>
+        /// <param name="path"></param>
+        public void ChangePath(string path)
+        {
+            this.Path = path;
+        }
+
+        /// Un filtro que retorna una copia de la imagen recibida y la guarda en un directorio.
+        /// </summary>
+        /// <param name="image">La imagen a la cual se le va a aplicar el filtro.</param>
+        /// <returns>La imagen recibida en un directorio indicado.</returns>
+        public IPicture Filter(IPicture image)
+        {
+            IPicture result = image.Clone();
+            
+            PictureProvider p = new PictureProvider();
+            p.SavePicture(result, Path);
+
+            return result;
+        }
+    }
+}
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,9 +8,11 @@ namespace CompAndDel
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            //Ejercicio 1
+
             PictureProvider p = new PictureProvider();
-            IPicture picture = p.GetPicture(@"C:\Users\FIT\Desktop\Git2\libros\PII_Pipes_Filters_RodrigoC\src\Program\luke.jpg");
+            IPicture picture = p.GetPicture(@"C:\Users\FIT\Desktop\Git2\libros\PII_Pipes_Filters_RodrigoC\src\Program\beer.jpg");
 
             PipeNull pipeNull = new PipeNull();
             FilterNegative filterNegative = new FilterNegative();
@@ -21,7 +23,34 @@ namespace CompAndDel
             IPicture finalimage = pipeSerialGreyscale.Send(picture);
             
             PictureProvider finalprovider = new PictureProvider();
-            finalprovider.SavePicture(finalimage, @"C:\Users\FIT\Desktop\Git2\libros\PII_Pipes_Filters_RodrigoC\src\Program\Filteredluke.jpg");
+            finalprovider.SavePicture(finalimage, @"C:\Users\FIT\Desktop\Git2\libros\PII_Pipes_Filters_RodrigoC\src\Program\Filteredbeer.jpg");
+            
+            //Ejercicio 2
+
+            PictureProvider p2 = new PictureProvider();
+            IPicture picture2 = p.GetPicture(@"C:\Users\FIT\Desktop\Git2\libros\PII_Pipes_Filters_RodrigoC\src\Program\luke.jpg");
+
+            PipeNull pipeNull2 = new PipeNull();
+            FilterSave filterSaveNegative2 = new FilterSave();
+            PipeSerial pipeSerialNegativeSave2 = new PipeSerial(filterSaveNegative2, pipeNull2);
+            FilterNegative filterNegative2 = new FilterNegative();
+            PipeSerial pipeSerialNegative2 = new PipeSerial(filterNegative, pipeSerialNegativeSave2);
+            FilterSave filterSaveGreyscale2 = new FilterSave();
+            PipeSerial pipeSerialGreyscaleSave2 = new PipeSerial(filterSaveGreyscale2, pipeSerialNegative2);
+            FilterGreyscale filterGreyscale2 = new FilterGreyscale();
+            PipeSerial pipeSerialGreyscale2 = new PipeSerial(filterGreyscale, pipeSerialGreyscaleSave2);
+            
+            filterSaveGreyscale2.ChangePath(@"C:\Users\FIT\Desktop\Git2\libros\PII_Pipes_Filters_RodrigoC\src\Program\GreyscaleLuke.jpg");
+            filterSaveNegative2.ChangePath(@"C:\Users\FIT\Desktop\Git2\libros\PII_Pipes_Filters_RodrigoC\src\Program\NegativeLuke.jpg");
+
+            IPicture finalimage2 = pipeSerialGreyscale2.Send(picture2);
+            
+            PictureProvider finalprovider2 = new PictureProvider();
+            finalprovider2.SavePicture(finalimage2, @"C:\Users\FIT\Desktop\Git2\libros\PII_Pipes_Filters_RodrigoC\src\Program\Filteredluke.jpg");
+
+            //Ejercicio 3
+            
+
         }
     }
 }
